2471555dfce6714a266d7d1187ed7142d94c3a52
Jeferson Vieira <jeff_malganis@hotmail.com>
2018-03-16 13:02:42 -0300

    adicionado app

--- app.rb
+++ app.rb
@@ -1,1 +1,12 @@
 # Input your code here
+require 'pry'
+require 'csv'
+require './lib/user'
+require './lib/args_validator'
+
+
+if ArgsValidator.args_valid?(ARGV)
+  if User.import_from_file(ARGV[1])
+    User.search(ARGV[2..-1])
+  end
+end
